913f7ba35b470c8ad575ac562eb3896bf2b76660
Jonathan de Jesus Martinez Sanchez <20231417@itla.edu.do>
2025-11-09 18:53:01 -0400

    Agregar modulo de asistencias

--- server.js
+++ server.js
@@ -23,12 +23,14 @@ const estudiantesRouter = require('./src/routes/estudiantes.router');
 const pagosRouter = require('./src/routes/pagos.router');
 const usuariosRouter = require('./src/routes/usuarios.router');
 const estudiantesRouter = require('./src/routes/estudiantes.router');
+const calificacionesRouter = require('./src/routes/calificaciones.router');
 
 app.use('/api/auth', authRouter);
 app.use('/api/estudiantes', estudiantesRouter);
 app.use('/api/pagos', pagosRouter);
 app.use('/api/usuarios', usuariosRouter);
 app.use('/api/estudiantes', estudiantesRouter);
+app.use('/api/calificaciones', calificacionesRouter);
 
 // middleware simple de error
 app.use((err, req, res, next) => {
